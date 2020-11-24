cfb122eddb53cd9ac5bf47cc3707b047bcaf5f88
Christopher Zell <christopher.zell@gmail.com>
2020-11-24 12:11:54 -0500

    initial

---
+++ src/main/java/mil/commonapi/demo/Controller.java
@@ -0,0 +1,13 @@
+package mil.commonapi.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/mil/commonapi/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package mil.commonapi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
